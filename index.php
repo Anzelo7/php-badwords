97a0b9750b7c3f3cfe830588410d75751ee03ac8
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-10-13 15:06:19 +0200

    push

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <?php
 
-    $object = $_GET["item"];
+$badWord = $_GET['word'];
 
-    $listaSpesa = 'Comprare ' . $object;
+//echo $badWord;
 
+$paragrafo = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates sapiente harum reprehenderit at voluptate tempore possimus numquam dicta laudantium, saepe mollitia commodi quidem dolores. Deserunt sequi libero repudiandae quidem consectetur?";
+
+echo '<div> Lunghezza paragrafo: ';
+
+echo strlen($paragrafo);
+
+echo '</div>';
+
+$censoredParagrafo = str_ireplace($badWord, '***', $paragrafo);
+
+$censoredLenght = strlen($censoredParagrafo);
 
 ?>
-<?php echo $listaSpesa ?>
-<?php
-
-    $none = str_replace($object, '***', $listaSpesa);
-
-?>
-
+<p>
+    <?= $paragrafo ?>
+</p>
 <div>
-    <?= $none ?>
+    Lunghezza censurata: <?= $censoredLenght ?>
 </div>
-
-<?php
-
-    var_dump($_GET);
-
-?>
+<p>
+    <?php echo $censoredParagrafo ?>
+</p>
